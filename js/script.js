9cbeea547c3d029e78f5e056809958e023fdefdb
aeror <aaronlaikh@gmail.com>
2016-01-25 04:21:15 -0800

    Setting up site test
    Basic site set up.  Only layout is complete, add images/thumbs and
    about/contact.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('#illust').on('click', function(){
+		window.location.replace('index.html');
+	});
+
+	$('#about').on('click', function(){
+		window.location.replace('about.html');
+	});
+
+	$('#contact').on('click', function(){
+		window.location.replace('contact.html');
+	});
+});
---
+++ js/script2.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	//$(".content").hide();
+});
+
+$("body").on('click', "#one", function(){
+	$(".content").toggle();
+	//window.location="illustration.html";
+});
